2f6000474d8f1a1cc929d423a77a9e4fb8d8d8e6
Nick Alexander <nckalx@yahoo.com>
2018-12-18 11:40:29 +0000

    regroup older versions together

--- v6/models/comment.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-var commentSchema = mongoose.Schema({
-    text: String,
-    author: String
-});
-
-
-module.exports = mongoose.model("Comment", commentSchema);

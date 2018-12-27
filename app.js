902b5d943ef89a3e5edb16f86b8191f236cbc244
Nick Alexander <nckalx@yahoo.com>
2018-12-27 18:35:38 +0000

    add mongolab

--- app.js
+++ app.js
@@ -18,8 +18,12 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     reviewRoutes        = require("./routes/reviews"),
     indexRoutes         = require("./routes/index");
-    
-mongoose.connect("mongodb://localhost:27017/camp_zone_v25", { useNewUrlParser: true});
+
+// Connect to MongoDB with Cloud 9    
+// mongoose.connect("mongodb://localhost:27017/camp_zone_v25", { useNewUrlParser: true});
+
+mongoose.connect("mongodb://nckalx:nicckk232@ds147233.mlab.com:47233/campzone", { useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
